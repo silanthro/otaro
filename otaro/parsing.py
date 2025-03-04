e3ea77cbe90103759ea61f47eaa4532c5ad3140b
SK <limsweekiat@gmail.com>
2025-03-04 13:02:27 +0800

    Fix non-escaped characters

--- otaro/parsing.py
+++ otaro/parsing.py
@@ -1,9 +1,12 @@
+import json
 import logging
 import re
+from itertools import combinations
 from typing import Any
 
 import dirtyjson
 from dirtyjson.attributed_containers import AttributedDict, AttributedList
+from fuzzywuzzy import process
 
 logging.basicConfig()
 logger = logging.getLogger("otaro.parsing")
@@ -32,8 +35,10 @@ def convert_attributed_container(
         return container
 
 
-def llm_parse_json(text: str):
+def llm_parse_json(text: str, keys: list[str] = None, autoescape=True):
     """Read LLM output and extract JSON data from it."""
+
+    keys = keys or []
 
     # First check for ```json
     code_snippet_pattern = r"```json(?P<json>(.|\s|\n)*?)```"
@@ -58,13 +63,20 @@ def llm_parse_json(text: str):
         ]
 
     # Try each result in order
+    result_json = None
     for result in results:
         if result is not None:
             try:
                 result_json = dirtyjson.loads(result)
-                return convert_attributed_container(result_json)
-            except dirtyjson.error.Error:
-                pass
+                break
+            except dirtyjson.error.Error as e:
+                if autoescape and e.msg.startswith("Expecting ',' delimiter"):
+                    # Possibly due to non-escaped quotes
+                    corrected_json_str = escape_quotes(result, keys)
+                    if corrected_json_str:
+                        result_json = dirtyjson.loads(corrected_json_str)
+                        break
+
             try:
                 result = (
                     result.replace("None", "null")
@@ -72,13 +84,58 @@ def llm_parse_json(text: str):
                     .replace("False", "false")
                 )
                 result_json = dirtyjson.loads(result)
-                return convert_attributed_container(result_json)
+                break
             except dirtyjson.error.Error:
                 continue
 
-    error_message = (
-        f"Failed to parse JSON from text {text!r}. Your model may not be capable of"
-        " supporting JSON output or our parsing technique could use some work. Try"
-        " a different model"
-    )
+    if result_json:
+        result_json = fuzzy_match_keys(result_json)
+        return convert_attributed_container(result_json)
+
+    error_message = f"Failed to parse JSON from text {text}"
     raise ValueError(error_message)
+
+
+# Brute force escape chars
+def escape_quotes(json_str: str, keys: list[str] = None):
+    keys = keys or []
+    quote_pos = [i for i, c in enumerate(json_str) if c in "\"'"]
+    # quit()
+    # At minimum there should be 2*len(keys) quotes, any quotes
+    # more than this is a candidate for escape
+    # In addition, as long as there is an escaped quote, we need
+    # at least two none-escaped quotes
+    # TODO: Stricter conditions
+    max_escapes = len(quote_pos) - 2 * len(keys) - 2
+    candidate_json_str = None
+    for n in range(1, max_escapes + 1):
+        candidates = list(combinations(quote_pos, n))
+        for candidate in candidates:
+            new_json_str = ""
+            for start, end in zip([0, *candidate], [*candidate, len(json_str)]):
+                new_json_str += json_str[start:end] + "\\"
+            new_json_str = new_json_str[:-1]
+            try:
+                parsed = llm_parse_json(new_json_str, keys, autoescape=False)
+                if all(key in parsed for key in keys):
+                    new_candidate = json.dumps(parsed)
+                    if candidate_json_str is None:
+                        candidate_json_str = new_candidate
+                    # Get the largest valid JSON
+                    elif len(new_candidate) > len(candidate_json_str):
+                        candidate_json_str = new_candidate
+            except Exception:
+                pass
+    return candidate_json_str
+
+
+def fuzzy_match_keys(json_dict: dict, gold_keys: list[str] = None, min_score=80):
+    if not gold_keys:
+        return json_dict
+    for key in json_dict.copy():
+        closest_key, score = process.extractOne(key, gold_keys)
+        if score == 100:
+            continue
+        elif score >= min_score:
+            json_dict[closest_key] = json_dict[key]
+    return json_dict
